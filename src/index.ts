6472c64ff8ec5d6779f7d1c0287b47ecf7157b1c
Dulysse <ulysseduponttravaux@gmail.com>
2025-03-14 10:48:00 +0100

    adding export symbols

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export type { Brand } from "./brands";
 export type { Infer } from "./applicators";
 export type { Helper } from "./helpers";
+export type { Sbl } from "./utils";
---
+++ src/utils/index.ts
@@ -0,0 +1,43 @@
+import type {
+	PrimaryKeyBrand,
+	ErrorBrand,
+	OmitBrand,
+	OptionalBrand,
+	PickBrand,
+	RequiredBrand,
+} from "./brands";
+
+/**
+ * List of `ts-branding` symbols:
+ * - {@link PrimaryKeyBrand}
+ * - {@link ErrorBrand}
+ * - {@link OmitBrand}
+ * - {@link OptionalBrand}
+ * - {@link PickBrand}
+ * - {@link RequiredBrand}
+ * Do you have any ideas or recommendations for improvement?
+ * ### Contact me!
+ * @author Ulysse Dupont -->
+ *  [my email](mailto:ulyssedupont2707@gmail.com)
+ *  | [my github](https://github.com/Dulysse)
+ *  | [my LinkedIn](https://www.linkedin.com/in/ulysse-dupont)
+ */
+export declare namespace Sbl {
+	export {
+		PrimaryKeyBrand,
+		ErrorBrand,
+		OmitBrand,
+		OptionalBrand,
+		PickBrand,
+		RequiredBrand,
+	};
+}
+
+export {
+	PrimaryKeyBrand,
+	ErrorBrand,
+	OmitBrand,
+	OptionalBrand,
+	PickBrand,
+	RequiredBrand,
+};
